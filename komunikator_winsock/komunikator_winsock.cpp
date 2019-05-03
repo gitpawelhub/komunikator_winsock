41eb0babc6e7f90e8da77eb9f7b54dfe488b812d
Piotr Dębowski <piotr.debowski01@student.wat.edu.pl>
2019-05-03 21:33:45 +0200

    Dodaj pliki projektów.

---
+++ komunikator_winsock/komunikator_winsock.cpp
@@ -0,0 +1,10 @@
+﻿// komunikator_winsock.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
